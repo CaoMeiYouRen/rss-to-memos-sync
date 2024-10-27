33f605e3b22c7008046006a075527afabde58883
CaoMeiYouRen <996881204@qq.com>
2024-10-27 21:01:34 +0800

    chore: 移除部分未使用的代码

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,20 +1,3 @@
 import { Api } from './memos'
 
-// export const memosApi = new Api({
-//     baseUrl: 'URL_ADDRESS',
-//     baseApiParams: {
-//         headers: {
-//             Authorization: `Bearer ${process.env.Authorization}`,
-//         },
-//     },
-// })
-
-// // 获取 memos 列表
-// export const getListMemos = memosApi.api.memoServiceListMemos
-
-// // 从 uid 获取 memo
-// export const getMemoByUid = memosApi.api.memoServiceGetMemoByUid
-
-// // 创建 memo
-// export const createMemo = memosApi.api.memoServiceCreateMemo
-
+export { Api }
